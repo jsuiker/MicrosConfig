46414476387d4e6af27a4ca9cb914cf007884cc4
Jonathan <jsuiker@vt.edu>
2017-08-31 16:30:39 -0400

    wrapping it up

--- index.ps1
+++ index.ps1
@@ -1,7 +1,9 @@
 iex ('regedit /s .\notifications\SecurityAndMaintenance-Disable.reg')
-iex ('regedit /s .\notifications\OneDrive-Disable.reg')
+Write-Host "Disabled Security and Maintenance Notifications"
 iex ('.\power\power_plan.ps1')
+Write-Host "Installed a new power plan (MicrosPOS)"
 iex ('.\services\services.ps1')
+Write-Host "Killed and disabled Windows Update and Windows Firewall"
 
 Write-Host "Press any key to continue..."
 $input = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
