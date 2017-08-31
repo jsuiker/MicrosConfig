2129576700424315a9fb88c1c06d8c2512e043c4
Jonathan <jsuiker@vt.edu>
2017-08-31 16:31:41 -0400

    and one

--- index.ps1
+++ index.ps1
@@ -4,6 +4,6 @@ iex ('.\power\power_plan.ps1')
 Write-Host "Installed a new power plan (MicrosPOS)"
 iex ('.\services\services.ps1')
 Write-Host "Killed and disabled Windows Update and Windows Firewall"
-
-Write-Host "Press any key to continue..."
+Write-Host ""
+Write-Host "Don't forget to log out and log back in again..."
 $input = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
